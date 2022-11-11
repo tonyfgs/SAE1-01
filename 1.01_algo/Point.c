122a1313ad515ff090cce38b7898c0e45e9246d2
khbruneau <116087367+khbruneau@users.noreply.github.com>
2022-11-11 19:34:33 +0100

    Update Point.c

--- 1.01_algo/Point.c
+++ 1.01_algo/Point.c
@@ -1,5 +1,23 @@
+/**
+ *\file Point.c
+ *\brief ce fichier contient les fonctions permettant d'acheter des boissons et de participer à un concert
+ *\author Tony FAGES
+ *\date 15 octobre 2022
+ */
+
 #include "SAE.h"
 
+/**
+ *\brief cherche la position d'un adherent dans le tableau tabAdherent
+ *\param tabAdherent[] tableau trié des numéros de carte
+ *\param tabEtat[] tableau des état des cartes
+ *\param tabAge[] tableau de l'age de chaque adhérent
+ *\param tlog taille logique des tableaux
+ *\param adherent le numéro de l'adhérent cherché
+ *\param *age renvoie par pointeur l'age de l'adhérent cherché
+ *\return la position de l'adherent dans le tableau ou -1 en cas d'erreur
+ *\author Tony FAGES
+ */
 int Info_Adhérent(int tabAdherent[],int tabEtat[], int tabAge[], int tlog,int adherent,int *age)
 {
     int pos, code_retour;
@@ -19,6 +37,17 @@ int Info_Adhérent(int tabAdherent[],int tabEtat[], int tabAge[], int tlog,int a
     return pos;
 }
 
+/**
+ *\brief débite la carte d'un adhérent en fonction de l'achat de boissons
+ *\param tabAdherent[] tableau trié des numéros de carte
+ *\param tabEtat[] tableau des état des cartes
+ *\param tabAge[] tableau de l'age de chaque adhérent
+ *\param tlog taille logique des tableaux
+ *\param adherent le numéro de l'adhérent débité
+ *\param *nb_soft renvoie par pointeur le nombre de boissons soft achetée au total dans une journée
+ *\param *nb_alcool renvoie par pointeur le nombre de boissons alcoolisées achetée au total dans une journée
+ *\author Tony FAGES
+ */
 void AchatBoissons(int tabAdherent[], int tabEtat[], int tabAge[],int tabCarte[], int tlog, int adherent,int *nb_soft,int *nb_alcool )
 {
     int limite = 0, code_ret, age, choix, prix_alcool = 13, prix_soft = 6, nombre, prix_tot;
@@ -48,12 +77,12 @@ void AchatBoissons(int tabAdherent[], int tabEtat[], int tabAge[],int tabCarte[]
           }
           if(choix == 1 && tabCarte[code_ret] < 13)
           {
-              printf("coucou Vous n'avez pas assez de point sur votre carte veuillez la recharger \n");
+              printf("Vous n'avez pas assez de point sur votre carte veuillez la recharger \n");
               break;
           }
           if(choix == 1 && limite > 5)
           {
-              printf("Vous ne pouvez achater que 5 boissons alcoolisée par jour. Achat Impossible \n");
+              printf("Vous ne pouvez acheter que 5 boissons alcoolisée par jour. Achat Impossible \n");
               break;
           }
         }
@@ -74,12 +103,22 @@ void AchatBoissons(int tabAdherent[], int tabEtat[], int tabAge[],int tabCarte[]
     }
 }
 
+/**
+ *\brief débite la carte d'un adhérent en fonction de l'achat de place de concert
+ *\param tabAdherent[] tableau trié des numéros de carte
+ *\param tabEtat[] tableau des état des cartes
+ *\param tabAge[] tableau de l'age de chaque adhérent
+ *\param tlog taille logique des tableaux
+ *\param adherent le numéro de l'adhérent débité
+ *\param *activité_concert renvoie par pointeur le nombre total de place acheté dans une journée 
+ *\author Tony FAGES
+ */
 void Concert(int tabAdherent[], int tabEtat[], int tabAge[],int tabCarte[], int tlog, int adherent, int *activité_concert)
 {
     int code_ret, age, choix, prix_entree = 66, prix_reduc = 40, prix_tot, nbplace ;
     code_ret = Info_Adhérent(tabAdherent, tabEtat, tabAge, tlog,adherent, &age);
     if (code_ret == -1) return; 
-    printf("Voulez-vous participer au concert des Eagles pour la somme de %d points(sauf si vous pouvez bénéfiez des reducs) :  (1=oui/0=non)i\n",prix_entree);
+    printf("Voulez-vous participer au concert des Eagles pour la somme de %d points (sauf si vous pouvez bénéficiez des reducs) :  (1=oui/0=non)i\n",prix_entree);
     scanf("%d",&choix);
     printf("Entrer le nombre de place que vous voulez : \n");
     scanf("%d",&nbplace);
@@ -128,7 +167,7 @@ void Concert(int tabAdherent[], int tabEtat[], int tabAge[],int tabCarte[], int 
 
         *activité_concert = *activité_concert + nbplace;
 
-    printf("Voulez-vous participer a d'autre concert pour la somme de %d points(sauf si vous pouvez bénéfiez des reducs) :  (1=oui/0=non)i\n",prix_entree);
+    printf("Voulez-vous participer a d'autre concert pour la somme de %d points (sauf si vous pouvez bénéficiez des reducs) :  (1=oui/0=non)i\n",prix_entree);
     scanf("%d",&choix);
     printf("Entrer le nombre de place que vous voulez : \n");
     scanf("%d",&nbplace);
